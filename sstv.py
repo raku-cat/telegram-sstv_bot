20d628183ceba340bfb1e5b78939d7bf93302fda
Raku-cat <raku@rakutiki.tv>
2016-08-24 17:06:52 -0500

    Fixed some dumb mistakes, cleaned a bit more, added more vorbosity for user

--- sstv.py
+++ sstv.py
@@ -13,21 +13,25 @@ def handle(msg):
     print(content_type, chat_type, chat_id)
 
     if content_type == 'photo':
+        bot.sendMessage(chat_id, 'Downloading...')
         file_id = msg['photo'][-1]['file_id']
         pic = bot.getFile(file_id)
         file_path = pic['file_path']
-        file_url = 'https://api.telegram.org/file/bot' + TOKEN + '/' + thisisit
+        file_url = 'https://api.telegram.org/file/bot' + TOKEN + '/' + file_path
         urllib.request.urlretrieve(file_url, file_id + '.jpg')
         img = Image.open(file_id + '.jpg')
         size = 320, 256
         rondo = randint(0, 100)
         img.thumbnail(size, Image.ANTIALIAS)
         img.save(file_id + '.jpg', 'JPEG')
+        bot.sendMessage(chat_id, 'Generating encoding...')
         subprocess.call(['pisstvpp', '-pm2', file_id + '.jpg'])
+        bot.sendMessage(chat_id, 'Distorting encoding...')
         distrt = ','.join(map(str, [randint(1,4), randint(1,300), randint(1,8)]))
         finald = bytes(distrt, 'utf-8')
         app = pysox.CSoxApp(file_id + '.jpg.wav', file_id + '.wav', effectparams=[ ("overdrive", [bytes(str(randint(1,100)), 'utf-8'), bytes(str(randint(1,100)), 'utf-8')] ), ("bend", [finald]), ])
         app.flow()
+        bot.sendMessage(chat_id, 'Sending...')
         bot.sendChatAction(chat_id, 'upload_audio')
         bot.sendAudio(chat_id, open(file_id + '.wav', 'rb'), title=file_id, duration=61)
         for file in glob.glob(file_id + '*'):
