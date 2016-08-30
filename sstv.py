047c6dd52c0f634c75befc2d8fada0bcb68d2c68
Raku-cat <raku@rakutiki.tv>
2016-08-29 19:37:52 -0500

    pysstv being too much of butt

--- sstv.py
+++ sstv.py
@@ -8,7 +8,6 @@ from PIL import Image
 from random import randint
 import pysox
 from datetime import datetime
-from pysstv.color import MartinM2
 
 TOKEN = open('token.txt', 'r').read()
 bot = telepot.Bot(TOKEN)
@@ -26,9 +25,10 @@ def handle(msg):
 		file_url = 'https://api.telegram.org/file/bot' + TOKEN + '/' + bot.getFile(file_id)['file_path']
 		urllib.request.urlretrieve(file_url, file_id + '.jpg')
 		img = Image.open(file_id + '.jpg')
-		img.resize([320, 256], Image.ANTIALIAS).save(file_id + '.jpg', 'JPEG')
+		img.thumbnail([320, 256], Image.ANTIALIAS)
+		img.save(file_id + '.jpg', 'JPEG')
 		bot.editMessageText(msg_ider, 'Generating encoding...')
-		MartinM2(img, 28000, 16).write_wav(file_id + '.jpg.wav')
+		subprocess.call(['pisstvpp', '-pm2', file_id + '.jpg'])
 		bot.editMessageText(msg_ider, 'Distorting encoding...')
 		pysox.CSoxApp(file_id + '.jpg.wav', curtime + '.wav', effectparams=[ ("overdrive", [bytes(str(randint(60,100)), 'utf-8'), bytes(str(randint(1,40)), 'utf-8')] ), ("ladspa", [b'/usr/lib/ladspa/satan_maximiser_1408.so', bytes(str(randint(-90,-10)), 'utf-8'), bytes(str(randint(-100,-10)), 'utf-8')] ), ("ladspa", [b'/usr/lib/ladspa/valve_rect_1405.so', bytes(str(randint(1,100)), 'utf-8'), bytes(str(randint(1,100)), 'utf-8')] ), ("ladspa", [b'/usr/lib/ladspa/retro_flange_1208.so', bytes(str(randint(1,50)), 'utf-8'), bytes(str(randint(-500,90)), 'utf-8')] ),]).flow()
 		bot.editMessageText(msg_ider, 'Sending...')
